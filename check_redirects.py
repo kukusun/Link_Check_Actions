9ae6a026bb3fdfa5ad37b7f3d52ae04cd1a6fa5d
K-_-SUN <kukusun@outlook.com>
2024-10-18 08:12:35 +0800

    Update check_redirects.py

--- check_redirects.py
+++ check_redirects.py
@@ -3,6 +3,7 @@ import requests
 # 在这里添加你收藏的链接
 urls = [
     "https://kukusun.github.io/blog/",
+    "https://github.com/kukusun/Link_Check_Actions/actions/workflows/",
     "https://tmp-hd106.vx-cdn.com/file-670fadd53483d-670fae4b71ea3/Windows6.1-KB3102810-x64.msu",
     "https://kukusun.github.io/blog",
     "https://potplayer.daum.net",
