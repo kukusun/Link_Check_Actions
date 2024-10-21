8433471dc3170d2bfcf2d17e9a6079c9b4b658b9
K-_-SUN <kukusun@outlook.com>
2024-10-21 12:02:46 +0800

    Update check_redirects.py

--- check_redirects.py
+++ check_redirects.py
@@ -100,10 +100,10 @@ urls = [
     "https://httpstat.us/561"
 ]
 
-headers = {
+headers = [
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0"
-}
+]
 
 def check_urls(urls):
     results = []
