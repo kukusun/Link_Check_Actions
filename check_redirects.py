9c47626fc5243c93db4d3466b8e5914c633eae15
K-_-SUN <kukusun@outlook.com>
2024-10-21 11:44:27 +0800

    Update check_redirects.py

--- check_redirects.py
+++ check_redirects.py
@@ -100,11 +100,16 @@ urls = [
     "https://httpstat.us/561"
 ]
 
+headers = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0"
+}
+
 def check_urls(urls):
     results = []
     for url in urls:
         try:
-            response = requests.get(url, allow_redirects=True)
+            response = requests.get(url, allow_redirects=True, headers=headers)
             final_url = response.url.rstrip('/')
             original_url = url.rstrip('/')
 
